69703bb4a1f60d862d520a25c7c45d8b6cd08338
Anastasia Pomeranets <anapomerash@gmail.com>
2024-10-11 23:51:14 +0300

    Move run_dna_rna_tools function to main script bio_utils.py as per assignment requirements

--- bio_utils.py
+++ bio_utils.py
@@ -1,7 +1,47 @@
 # bio_utils.py
 
-from .dna_rna.dna_rna_tools import run_dna_rna_tools
 from .fastq.fastq_utils import filter_fastq
 from .translation.translation_utils import translate_rna
 
 __all__ = ["run_dna_rna_tools", "filter_fastq", "translate_rna"]
+
+def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
+    """
+    Выполняет указанную процедуру
+    над переданными последовательностями ДНК или РНК.
+
+    :param args: Произвольное количество строковых аргументов,
+        где последний аргумент — название процедуры.
+    :return: Результат выполнения процедуры
+        в виде строки или списка строк.
+    :raises ValueError: Если последовательность некорректна
+        или процедура неизвестна.
+    """
+    if len(args) < 2:
+        raise ValueError(
+            """Необходимо передать хотя бы одну последовательность и
+            название процедуры."""
+        )
+
+    procedure = args[-1]
+    sequences = args[:-1]
+    results = []
+
+    functions = {
+        "transcribe": transcribe,
+        "reverse": reverse,
+        "complement": complement,
+        "reverse_complement": reverse_complement,
+    }
+
+    if procedure not in functions:
+        raise ValueError(f"Неизвестная процедура: {procedure}")
+
+    for seq in sequences:
+        result = functions[procedure](seq)
+        results.append(result)
+
+    if len(results) == 1:
+        return results[0]
+    else:
+        return results
--- dna_rna/dna_rna_tools.py
+++ dna_rna/dna_rna_tools.py
@@ -15,48 +15,6 @@ COMP_DICT = {
 }
 
 
-def run_dna_rna_tools(*args: str) -> Union[str, List[str]]:
-    """
-    Выполняет указанную процедуру
-    над переданными последовательностями ДНК или РНК.
-
-    :param args: Произвольное количество строковых аргументов,
-        где последний аргумент — название процедуры.
-    :return: Результат выполнения процедуры
-        в виде строки или списка строк.
-    :raises ValueError: Если последовательность некорректна
-        или процедура неизвестна.
-    """
-    if len(args) < 2:
-        raise ValueError(
-            """Необходимо передать хотя бы одну последовательность и
-            название процедуры."""
-        )
-
-    procedure = args[-1]
-    sequences = args[:-1]
-    results = []
-
-    functions = {
-        "transcribe": transcribe,
-        "reverse": reverse,
-        "complement": complement,
-        "reverse_complement": reverse_complement,
-    }
-
-    if procedure not in functions:
-        raise ValueError(f"Неизвестная процедура: {procedure}")
-
-    for seq in sequences:
-        result = functions[procedure](seq)
-        results.append(result)
-
-    if len(results) == 1:
-        return results[0]
-    else:
-        return results
-
-
 def transcribe(sequence: str) -> str:
     """
     Транскрибирует ДНК-последовательность в РНК.
